b1c45b630b6ff9e809ad3949718e91ed73f15ac5
campcodebrasil <jhmiante@outlook.com>
2023-12-17 22:48:41 -0300

    Deixando nosso APP seguro

---
+++ create_secret.py
@@ -0,0 +1,5 @@
+import secrets
+
+sk = secrets.token_hex(36)
+
+print(sk)
--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'AS56D1A65F16SA5D1FG52677-A1243!!S65DF1AS561FDA5'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
